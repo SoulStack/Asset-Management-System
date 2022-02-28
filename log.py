fcdd3fd9e0e8896928ca5f1330bdac359c7e4877
SoulStack <sivashreet.maharana@soulunileaders.com>
2022-02-28 09:55:11 +0000

    main

--- log.py
+++ log.py
@@ -1,10 +1,12 @@
 import main
 import time
 n=0
+c=[]
 reader1 = main.Reader("192.168.0.250",27011,"13.76.182.251","reader1")
 while n<=10:
 	b=reader1.scan_tag_capture()
-	print(b)
+
+	print(c)
 	time.sleep(6)
 	n+=1
 print(reader1.reader_status())
