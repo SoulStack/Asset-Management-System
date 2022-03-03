fdab12e284de00e10646c0a63973bc36e605cb7c
SoulStack <sivashreet.maharana@soulunileaders.com>
2022-03-03 06:35:41 +0000

    main

--- testing/log.py
+++ testing/log.py
@@ -1,35 +1,8 @@
-import logging
-import pyodbc
+import main
 import time
-
-logging.basicConfig(filename= "newfile.log",format = "%(asctime)s %(message)s",filemode = "w")
-
-#creating an object
-
-logger = logging.getLogger()
-
-#setting the threshold of logger
-
-logger.setLevel(logging.DEBUG)
-
-
-def get() :
-    t1 = time.time()
-    server =  'soulasset.database.windows.net'
-    database = "asset"
-    username = "assetadmin"
-    password = "Soulsvciot01"
-
-    cnxn =  pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-
-    logger.debug("connected to database")
-    cursor = cnxn.cursor()
-    cursor.execute("SELECT * FROM History")
-    row = cursor.fetchone()
-
-    while row:
-        print(row[1])
-    row = cursor.fetchone()
-    t2 = time.time()
-    print("Total time taken{}".format(t2 - t1))
-
+n=0
+reader1 = main.Reader("192.168.0.250",27011,"13.76.182.251","reader1")
+while True:
+	b=reader1.scan_tag_capture()
+	print(map(reader1.hex_to_string,b))
+	#time.sleep(1)
